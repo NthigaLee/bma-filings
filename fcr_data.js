457b3a3ede58341ef28f389a4b62e0b3699ceb1b
Lee <carljlee047@gmail.com>
2026-06-14 11:42:13 +0000

    Add BMA filing comparison hub + fix solvency page bugs
    - Add hub_index.html (Market Overview)
    - Add hub_company.html (Company Profiles)
    - Add hub_compare.html (Head-to-Head Compare)
    - Add hub_solvency.html (Solvency/FCR) with bug fixes:
      * Fix equityRatioChart collapse (add explicit card height)
      * Remove unloaded annotation plugin reference
      * Filter null-only FCR entries from table
    - Add fcr_data.js (ECR/BSCR solvency data from FCRs)
    - Update dashboard_data.js and dashboard_data.json (40 companies, 2019-2024)

---
+++ fcr_data.js
@@ -0,0 +1,77 @@
+var fcrData = {
+  "Arch Reinsurance": {
+    "entity_label": "Arch Reinsurance Ltd. (ARL)",
+    "2024": {
+      "ecr_ratio": 329,
+      "ecr_amount": 8482,
+      "eligible_capital": 27880,
+      "msm_ratio": null
+    },
+    "2023": {
+      "ecr_ratio": null,
+      "ecr_amount": 6125,
+      "eligible_capital": 24820,
+      "msm_ratio": null
+    }
+  },
+  "XL Bermuda": {
+    "entity_label": "XL Bermuda Ltd.",
+    "2024": {
+      "ecr_ratio": 186,
+      "ecr_amount": 7762,
+      "eligible_capital": 14437,
+      "msm_ratio": 728
+    },
+    "2023": {
+      "ecr_ratio": 176,
+      "ecr_amount": null,
+      "eligible_capital": 14200,
+      "msm_ratio": null
+    }
+  },
+  "Partner Reinsurance Company": {
+    "entity_label": "Partner Reinsurance Company Ltd. (Bermuda)",
+    "2024": {
+      "ecr_ratio": 285,
+      "ecr_amount": 2686,
+      "eligible_capital": 7505,
+      "msm_ratio": null
+    },
+    "2023": {
+      "ecr_ratio": 303,
+      "ecr_amount": 2344,
+      "eligible_capital": null,
+      "msm_ratio": null
+    }
+  },
+  "SiriusPoint Bermuda Insurance": {
+    "entity_label": "SiriusPoint Bermuda Insurance Company Ltd.",
+    "2024": {
+      "ecr_ratio": 257,
+      "ecr_amount": 1216,
+      "eligible_capital": 3131,
+      "msm_ratio": null
+    },
+    "2023": {
+      "ecr_ratio": 294,
+      "ecr_amount": 1240,
+      "eligible_capital": 3640,
+      "msm_ratio": null
+    }
+  },
+  "Fortitude Reinsurance Company": {
+    "entity_label": "Fortitude Reinsurance Company Ltd. (Group)",
+    "2024": {
+      "ecr_ratio": null,
+      "ecr_amount": null,
+      "eligible_capital": null,
+      "msm_ratio": null
+    },
+    "2023": {
+      "ecr_ratio": null,
+      "ecr_amount": null,
+      "eligible_capital": null,
+      "msm_ratio": null
+    }
+  }
+};
